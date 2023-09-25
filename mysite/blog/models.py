1c0e4596a466806b2f17431b16e4f7ba478d9b28
Semyon Shinkarenko <shinkarenkosema@gmail.com>
2023-09-25 15:51:05 +0300

    admin models imp, list & detail views added

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+            .filter(status=Post.Status.PUBLISHED)
 
 
 class Post(models.Model):
@@ -23,6 +30,9 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
