8b0caebf79f47bb6bb0da9bf8974c322e7b20c1b
Semyon Shinkarenko <shinkarenkosema@gmail.com>
2023-09-28 14:27:30 +0300

    add e-mail sending func

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'shinkarenkosema@gmail.com'
+EMAIL_HOST_PASSWORD = 'ghni mhtl brsf irkc'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
