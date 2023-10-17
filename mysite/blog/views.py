8c689285eacf52cab9075d4644364700c6eb1a95
Semyon Shinkarenko <shinkarenkosema@gmail.com>
2023-10-17 08:48:31 +0300

    search func add

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,10 +3,13 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.contrib.postgres.search import SearchVector, \
+    SearchQuery, SearchRank
+
 # Create your views here.
 
 
@@ -103,3 +106,25 @@ def post_detail(request, year, month, day, post):
                    'comments': comments,
                    'form': form,
                    'similar_posts': similar_posts})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
