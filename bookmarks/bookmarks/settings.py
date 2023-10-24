29b0049e298684c98329f6050bb5715f9f892729
Semyon Shinkarenko <shinkarenkosema@gmail.com>
2023-10-24 16:47:05 +0300

    profile edit func added

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -129,3 +129,12 @@ LOGOUT_URL = 'logout'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
--- bookmarks/bookmarks/urls.py
+++ bookmarks/bookmarks/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
@@ -23,3 +25,8 @@ urlpatterns = [
     path('account/', include('account.urls')),
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
